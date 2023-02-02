7c3c5f11fded1da890c08159f0526251b58baf47
BrunodMC <78260433+BrunodMC@users.noreply.github.com>
2023-02-02 17:13:18 +0100

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -34,7 +34,7 @@ def cleanup() -> None:
     # remove any entries with no sale price
     df.drop(df[df['Sale price'].isnull()].index, inplace=True)
     # convert sale price into an integer value
-    df['Sale price'] = df['Sale price'].apply(_str_to_int)
+    df['Sale price'] = df['Sale price'].apply(_str_to_float)
     # remove entries with sale price below 200k (don't want to include garages)
     cutoff = 200000
     df.drop(df[df['Sale price'] < cutoff].index, inplace=True)
@@ -48,7 +48,6 @@ def cleanup() -> None:
     # replace NaNs in num of bedrooms with median value (2), and convert to int
     num_bedrooms_median = df['Number of bedrooms'].median()
     df.loc[df['Number of bedrooms'].isnull(), 'Number of bedrooms'] = num_bedrooms_median
-    df['Number of bedrooms'] = df['Number of bedrooms'].astype(int)
 
     # for year of construction:
     # fix entries from braindead people (e.g., year of construction: 12)
@@ -65,10 +64,10 @@ def cleanup() -> None:
     if 'Indoor parking space(s)' and 'Closed parking space' in df.columns:
         df['Closed parking space'] = df['Closed parking space'].fillna(df['Indoor parking space(s)'])
         df.drop('Indoor parking space(s)', axis=1, inplace=True)
-    df['Closed parking space'] = df['Closed parking space'].apply(_nan_to_int)
+    df['Closed parking space'] = df['Closed parking space'].apply(_nan_to_float)
 
     # convert Open parking space into int, turning NaNs into 0 and 'Yes' into 1
-    df['Open parking space'] = df['Open parking space'].apply(_yesint_to_int)
+    df['Open parking space'] = df['Open parking space'].apply(_yesint_to_float)
 
     # convert Energy class NaNs into 'NC' and strip the numbers from those that have them
     df['Energy class'] = df['Energy class'].apply(_energy_class_convert)
@@ -77,64 +76,64 @@ def cleanup() -> None:
     df['Thermal insulation class'] = df['Thermal insulation class'].apply(_energy_class_convert)
 
     # convert Open kitchen into binary int
-    df['Open kitchen'] = df['Open kitchen'].apply(_binary_int)
+    df['Open kitchen'] = df['Open kitchen'].apply(_binary_cat)
 
     # merge values from 'Bathooms' into the Bathroom column and convert NaNs into 0
     if 'Bathooms' and 'Bathroom' in df.columns:
         df['Bathroom'] = df['Bathroom'].fillna(df['Bathooms'])
         df.drop('Bathooms', axis=1, inplace=True)
-    df['Bathroom'] = df['Bathroom'].apply(_nan_to_int)
+    df['Bathroom'] = df['Bathroom'].apply(_nan_to_float)
 
     # convert Basement column into binary int
-    df['Basement'] = df['Basement'].apply(_binary_int)
+    df['Basement'] = df['Basement'].apply(_binary_cat)
 
     # convert Property's floor NaNs into 0's and the rest into int
-    df['Property\'s floor'] = df['Property\'s floor'].apply(_nan_to_int)
+    df['Property\'s floor'] = df['Property\'s floor'].apply(_nan_to_float)
 
     # convert Lift column to binary int
-    df['Lift'] = df['Lift'].apply(_binary_int)
+    df['Lift'] = df['Lift'].apply(_binary_cat)
 
     # convert Fitted kitchen column to binary int
-    df['Fitted kitchen'] = df['Fitted kitchen'].apply(_binary_int)
+    df['Fitted kitchen'] = df['Fitted kitchen'].apply(_binary_cat)
     
     # convert Separate kitchen column to binary int
-    df['Separate kitchen'] = df['Separate kitchen'].apply(_binary_int)
+    df['Separate kitchen'] = df['Separate kitchen'].apply(_binary_cat)
 
     # convert Restroom column into int
-    df['Restroom'] = df['Restroom'].apply(_yesint_to_int)
+    df['Restroom'] = df['Restroom'].apply(_yesint_to_float)
 
     # convert Laundry column to binary int
-    df['Laundry'] = df['Laundry'].apply(_binary_int)
+    df['Laundry'] = df['Laundry'].apply(_binary_cat)
 
     # combine 'Shower room' and 'Shower rooms' into one column, int
     if 'Shower rooms' and 'Shower room' in df.columns:
         df['Shower rooms'] = df['Shower rooms'].fillna(df['Shower room'])
         df.drop('Shower room', axis=1, inplace=True)
-    df['Shower rooms'] = df['Shower rooms'].apply(_nan_to_int)
+    df['Shower rooms'] = df['Shower rooms'].apply(_nan_to_float)
 
     # convert Balcony column into float
     df['Balcony'] = df['Balcony'].apply(_str_to_float)
 
     # convert Pets accepted column to binary int
-    df['Pets accepted'] = df['Pets accepted'].apply(_binary_int)
+    df['Pets accepted'] = df['Pets accepted'].apply(_binary_cat)
 
     # convert Swimming pool column to binary int
-    df['Swimming pool'] = df['Swimming pool'].apply(_binary_int)
+    df['Swimming pool'] = df['Swimming pool'].apply(_binary_cat)
 
     # convert Sauna column to binary int
-    df['Sauna'] = df['Sauna'].apply(_binary_int)
+    df['Sauna'] = df['Sauna'].apply(_binary_cat)
 
     # convert Solar panels column to binary int
-    df['Solar panels'] = df['Solar panels'].apply(_binary_int)
+    df['Solar panels'] = df['Solar panels'].apply(_binary_cat)
 
     # convert Garden column into float
     df['Garden'] = df['Garden'].apply(_str_to_float)
 
     # convert Fireplace column to binary int
-    df['Fireplace'] = df['Fireplace'].apply(_binary_int)
+    df['Fireplace'] = df['Fireplace'].apply(_binary_cat)
 
     # convert Attic column to binary int
-    df['Attic'] = df['Attic'].apply(_binary_int)
+    df['Attic'] = df['Attic'].apply(_binary_cat)
 
     
     # drop all other columns
@@ -154,29 +153,26 @@ def cleanup() -> None:
 
 
 ### Support functions
-def _nan_to_int(x):
+def _nan_to_float(x):
     if pd.isnull(x) or x > 50:
-        return 0
+        return 0.
     else:
-        return int(x)
-
-def _str_to_int(x):
-    try:
-        return int(str(x).split(' ')[0].replace(',', ''))
-    except:
-        return 0
+        return float(x)
 
 def _str_to_float(x):
     if pd.isnull(x):
-        return 0.0
+        return 0.
     else:
-        return float(x.split(' ')[0].replace(',', ''))
+        try:
+            return float(x.split(' ')[0].replace(',', ''))
+        except:
+            return 0.
 
-def _binary_int(x):
+def _binary_cat(x):
     if pd.isnull(x):
-        return 0
+        return 0.
     else:
-        return 1
+        return 1.
 
 def _energy_class_convert(x):
     if pd.isnull(x) or x == 'NS':
@@ -184,13 +180,13 @@ def _energy_class_convert(x):
     else:
         return ''.join(filter(str.isalpha, x))
 
-def _yesint_to_int(x):
+def _yesint_to_float(x):
     if pd.isnull(x):
-        return 0
+        return 0.
     elif x == 'Yes':
-        return 1
+        return 1.
     else:
-        return int(x)
+        return float(x)
 
 
 
